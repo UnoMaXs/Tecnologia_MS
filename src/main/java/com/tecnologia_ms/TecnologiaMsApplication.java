aee47a8f5caf9e37e3c158b36816e50874ec4f24
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-06-09 11:34:32 -0500

    Proyecto Base

---
+++ src/main/java/com/tecnologia_ms/TecnologiaMsApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologia_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnologiaMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecnologiaMsApplication.class, args);
+    }
+
+}
